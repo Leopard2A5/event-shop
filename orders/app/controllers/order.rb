6cae48bcceb3c245b428a7a1b141cbd3f01628d3
René Perschon <rene.perschon@payu.com>
2017-03-27 13:32:58 +0200

    added creation of orders

--- orders/app/controllers/order.rb
+++ orders/app/controllers/order.rb
@@ -6,4 +6,13 @@ Orders::App.controllers '/orders', :produces => :json do
     Order.all.to_json
   end
 
+  post '/' do
+    order = Order.new(params)
+    if order.valid?
+      order.save.to_json
+    else
+      [400, "Invalid request"]
+    end
+  end
+
 end
--- orders/models/order.rb
+++ orders/models/order.rb
@@ -1,4 +1,4 @@
 class Order < ActiveRecord::Base
   validates :customer_id, presence: true, length: { minimum: 1 }
-  validates :amount, numericality: { greater_than: 0.0 }
+  validates :amount, presence: true, numericality: { greater_than: 0.0 }
 end
