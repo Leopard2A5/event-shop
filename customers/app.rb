f5c10a4e5e5efa4241e5d92640ab43fc628a043f
René Perschon <rene.perschon@payu.com>
2017-03-28 09:06:09 +0200

    customer service now listens for created orders and responds

--- customers/app.rb
+++ customers/app.rb
@@ -1,9 +1,40 @@
 #!/usr/bin/env ruby
 
-require 'sinatra'
 require 'json'
+require 'bunny'
 
-get '/' do
-  content_type :json
-  [200, {foo: 'bar'}.to_json]
+def connect(conn)
+  attempts = 5
+  begin
+    conn.start
+  rescue Bunny::TCPConnectionFailedForAllHosts => e
+    attempts -= 1
+    if attempts > 0
+      sleep(3)
+      retry
+    else
+      raise e
+    end
+  end
+end
+
+conn = Bunny.new(:hostname => 'rabbitmq')
+connect(conn)
+
+ch = conn.create_channel
+q_orders_created = ch.queue('orders_created')
+q_orders_credited = ch.queue('orders_credited')
+q_orders_declined = ch.queue('orders_declined')
+
+STDERR.puts "Listening on queue orders_created"
+q_orders_created.subscribe(:block => true) do |delivery_info, properties, body|
+  STDERR.puts "Received #{body}"
+  body = JSON.parse(body)
+  if body['amount'].to_f < 50.0
+    STDERR.puts "Order credited"
+    ch.default_exchange.publish(body['id'], :routing_key => q_orders_credited.name)
+  else
+    STDERR.puts "Order declined"
+    ch.default_exchange.publish(body['id'], :routing_key => q_orders_declined.name)
+  end
 end
