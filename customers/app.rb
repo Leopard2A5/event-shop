52f8d19b202945d1c17e64c4e7564d7c452897f2
René Perschon <rene.perschon@payu.com>
2017-04-10 15:24:54 +0200

    commented in order rejection

--- customers/app.rb
+++ customers/app.rb
@@ -25,17 +25,17 @@ connect(conn)
 ch = conn.create_channel
 q_orders_created = ch.queue('orders_created')
 q_orders_credited = ch.queue('orders_credited')
-# q_orders_declined = ch.queue('orders_declined')
+q_orders_declined = ch.queue('orders_declined')
 
 STDERR.puts "Listening on queue orders_created"
 q_orders_created.subscribe(:block => true) do |delivery_info, properties, body|
   STDERR.puts "Received #{body}"
   body = JSON.parse(body)
-  # if body['amount'].to_f < 50.0
+  if body['amount'].to_f < 50.0
     STDERR.puts "Order credited: #{q_orders_credited.inspect}"
     ch.default_exchange.publish(body['id'].to_json, :routing_key => q_orders_credited.name)
-  # else
-    # STDERR.puts "Order declined"
-    # ch.default_exchange.publish(body['id'].to_json, :routing_key => q_orders_declined.name)
-  # end
+  else
+    STDERR.puts "Order declined"
+    ch.default_exchange.publish(body['id'].to_json, :routing_key => q_orders_declined.name)
+  end
 end
