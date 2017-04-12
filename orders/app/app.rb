2b31674fa32c89fc4f04d9729b3ea15e7901d0d1
René Perschon <rperschon85@gmail.com>
2017-04-12 21:51:01 +0200

    added automatic application/json parsing

--- orders/app/app.rb
+++ orders/app/app.rb
@@ -1,6 +1,14 @@
+require 'rack/parser'
+
 module Orders
   class App < Padrino::Application
     use ConnectionPoolManagement
     register RabbitMQInitializer
+
+    use Rack::Parser, content_types: {
+      'application/json': Proc.new do |body|
+        ::MultiJson.decode body
+      end
+    }
   end
 end
